aa6a09c927327a59eee14e7c3236bebd698b813f
Luis Arias <kaaloo@gmail.com>
2013-09-05 09:10:56 +0200

    Update class name here.

--- PinPaymentsGrailsPlugin.groovy
+++ PinPaymentsGrailsPlugin.groovy
@@ -1,4 +1,4 @@
-class SpreedlyGrailsPlugin {
+class PinPaymentsGrailsPlugin {
     // the plugin version
     def version = "0.2"
     // the version or versions of Grails the plugin is designed for
